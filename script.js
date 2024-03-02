ec423ba7041339f0f35f8773870ec70bdb039215
Zohaib Ali <156583311+ZohaibAli145@users.noreply.github.com>
2024-03-02 20:35:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// ==== navbar toggle === 
+let navBar = document.querySelector('.navLink');
+let menuBar = document.querySelector('#menuBtn');
+
+menuBar.onclick = () => {
+    navBar.classList.toggle('active');
+}
+
+// == scroll section and sticky navbar == 
+window.onscroll = () => {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrolly > 100)
+}
+
+// *** swiper js ****
+var swiper = new Swiper(".myHome", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
